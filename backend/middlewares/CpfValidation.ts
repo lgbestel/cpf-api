0f7470507ee35e00a9ba099559767a674f9e0bc2
Luiz Guilherme Bestel <luizgbes@gmail.com>
2023-02-09 14:39:11 -0300

    add: cpf validation

---
+++ backend/middlewares/CpfValidation.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default class CpfValidation {
+  validateLength = async (req: Request, res: Response, next: NextFunction) => {
+    const cpf = req.params.cpf || req.body.cpf;
+    console.log(cpf);
+    if (cpf.length !== 11) {
+      return res.status(404).json({ type: 'InvalidException', message: 'CPF is not valid.'});
+    }
+    return next();
+  }
+
+  validateDigits = async (req: Request, res: Response, next: NextFunction) => {
+    const cpf = req.params.cpf || req.body.cpf;
+    const cpfDigits = cpf.split('');
+    const summedCpfDigits = cpfDigits.map((num: string) => Number(num))
+      .reduce((acc: number, curr: number) => acc + curr);
+    if (summedCpfDigits / 11 === Number(cpfDigits[0])) {
+      return res.status(404).json({ type: 'InvalidException', message: 'CPF is not valid.'});
+    }
+    return next();
+  }
+}
--- backend/routes/CpfRoutes.ts
+++ backend/routes/CpfRoutes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
+import CpfValidation from '../middlewares/CpfValidation';
 
 import CpfController from '../controller/CpfController';
 const cpfController = new CpfController();
+const cpfValidation = new CpfValidation();
 
 const cpfRouter = Router();
 
-cpfRouter.post('/', cpfController.add);
-cpfRouter.get('/:cpf', cpfController.findOne);
-cpfRouter.delete('/:cpf', cpfController.remove);
 cpfRouter.get('/', cpfController.findAll);
+cpfRouter.get('/:cpf', cpfValidation.validateLength, cpfValidation.validateDigits, cpfController.findOne);
+cpfRouter.post('/', cpfValidation.validateLength, cpfValidation.validateDigits, cpfController.add);
+cpfRouter.delete('/:cpf', cpfValidation.validateLength, cpfValidation.validateDigits, cpfController.remove);
 
 export default cpfRouter;
